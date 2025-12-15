a2e3307abf6e01fa510d8d263ce9c31ab5892ab5
Nitai Sarkar <140227933+NitaiStudio@users.noreply.github.com>
2025-12-15 14:38:12 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "rikishort-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&family=Montserrat:wght@800&display=swap",
+  "https://fonts.googleapis.com/icon?family=Material+Icons+Round",
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js",
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js",
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch Requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update Service Worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
